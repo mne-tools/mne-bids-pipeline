fb6bcd0b997d8468361aec27e00f75f1b74b52e7
Joan Massich <sik@visor.udg.edu>
2019-01-30 10:58:55 +0100

    Add data fetcher

---
+++ 00-fetch_data.py
@@ -0,0 +1,11 @@
+"""
+================================
+00. Fetch MNE-Python sample data
+================================
+
+"""  # noqa: E501
+
+
+import mne
+
+mne.datasets.sample.data_path(update_path=True)

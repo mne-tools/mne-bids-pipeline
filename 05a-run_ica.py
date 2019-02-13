b1ed266faa6edd804863280c1559633baf23a9cf
Sophie Herbst <ksherbst@gmail.com>
2019-02-13 16:57:58 +0100

    run ICA on meg and eeg separately

--- 05a-run_ica.py
+++ 05a-run_ica.py
@@ -14,50 +14,56 @@ from mne.parallel import parallel_func
 
 import config
 
-# Here we always process with the 1 Hz highpass data (instead of using
-# l_freq) because ICA needs a highpass.
 
-
-grads_rejection_limit = 4000e-13
-mags_rejection_limit = 4e-12
 decim = 11  # do not touch this value unless you know what you are doing
-
 
 def run_ica(subject, tsss=config.mf_st_duration):
     print("Processing subject: %s" % subject)
-    
+
     meg_subject_dir = op.join(config.meg_dir, subject)
-   
+
     epochs_fname = op.join(meg_subject_dir,
-                            config.base_epochs_fname.format(**locals()))
+                           config.base_epochs_fname.format(**locals()))
     epochs_for_ICA_fname = op.splitext(epochs_fname)[0] + '_for_ICA.fif'
 
     epochs_for_ICA = mne.read_epochs(epochs_for_ICA_fname, preload=True)
 
-    # SSS reduces the data rank and the noise levels, so let's include
-    # components based on a higher proportion of variance explained (0.999)
-    # than we would otherwise do for non-Maxwell-filtered raw data (0.98)
-    n_components = 0.999  # XXX: This can bring troubles to ICA
+
     
-    ica_name = op.join(config.meg_dir, 
-                       '{0}_{1}-ica.fif'.format(subject, config.study_name))
+    # run ICA on MEG and EEG
+    picks_meg = mne.pick_types(epochs_for_ICA.info, meg=True, eeg=False,
+                               eog=False, stim=False, exclude='bads')
+    picks_eeg = mne.pick_types(epochs_for_ICA.info, meg=False, eeg=True,
+                               eog=False, stim=False, exclude='bads')
+    all_picks = dict({'meg': picks_meg, 'eeg': picks_eeg})
+
+    for ch_type in ['meg', 'eeg']:
+        print('Running ICA for ' + ch_type)
         
-    # Here we only compute ICA for MEG because we only eliminate ECG artifacts,
-    # which are not prevalent in EEG (blink artifacts are, but we will remove
-    # trials with blinks at the epoching stage).
-    print('  Fitting ICA')
-    ica = ICA(method='fastica', random_state=config.random_state,
+        if ch_type == 'meg': 
+            
+            # XXX this does not work on epochs: 
+            # meg_maxfilter_rank = epochs_for_ICA.copy().pick_types(meg=True).estimate_rank()
+            n_components=0.999
+            
+        elif ch_type == 'eeg': 
+            
+            n_components=0.999
+        
+        ica = ICA(method='fastica', random_state=config.random_state,
               n_components=n_components)
-    
-    # XXX run ICA on MEG and EEG
-    picks = mne.pick_types(epochs_for_ICA.info, meg=True, eeg=False, eog=False,
-                           stim=False, exclude='bads')
-    
-    ica.fit(epochs_for_ICA, picks=picks, decim=decim)
-    
-    print('  Fit %d components (explaining at least %0.1f%% of the variance)'
-          % (ica.n_components_, 100 * n_components))
-    ica.save(ica_name)
+        
+        picks = all_picks[ch_type]
+
+        ica.fit(epochs_for_ICA, picks=picks, decim=decim)
+
+        print('  Fit %d components (explaining at least %0.1f%% of the variance)'
+              % (ica.n_components_, 100 * n_components))
+
+        ica_name = op.join(meg_subject_dir,
+                           '{0}_{1}_{2}-ica.fif'.format(subject, config.study_name,
+                                                        ch_type))
+        ica.save(ica_name)
 
 
 # Memory footprint: around n_jobs * 4 GB

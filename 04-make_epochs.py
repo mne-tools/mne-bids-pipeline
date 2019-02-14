b54267875476403b6209b6aee389256e05c82a0d
Sophie Herbst <ksherbst@gmail.com>
2019-02-14 09:40:37 +0100

    prepare eochs for ICA in script 04a, don't save

--- 04-make_epochs.py
+++ 04-make_epochs.py
@@ -73,18 +73,6 @@ def run_epochs(subject):
 
     epochs.save(epochs_fname)
     
-    # produce high-pass filtered version of the data for ICA    
-    epochs_for_ica = mne.Epochs(raw.copy().filter(l_freq=1., h_freq=None),
-                                events, config.event_id, config.tmin, 
-                                config.tmax, proj=True, 
-                                picks=picks, baseline=config.baseline,
-                                preload=False, decim=config.decim,
-                                reject=config.reject)
-    
-    
-    epochs_for_ica_fname = op.splitext(epochs_fname)[0] + '_for_ica.fif'
-
-    epochs_for_ica.save(epochs_for_ica_fname)
 
 # Here we use fewer N_JOBS to prevent potential memory problems
 parallel, run_func, _ = parallel_func(run_epochs, n_jobs=N_JOBS)
--- 05a-run_ica.py
+++ 05a-run_ica.py
@@ -2,8 +2,11 @@
 ===========
 05. Run ICA
 ===========
-# XXX update
-ICA decomposition using fastICA.
+This fits ICA on epoched data filtered with 1 Hz highpass, 
+for this purpose only using fastICA. Separate ICAs are fitted and stored for 
+MEG and EEG data. 
+To actually remove designated ICA components from your data, you will have to 
+run 06a-apply_ica.py. 
 """
 
 import os.path as op
@@ -17,41 +20,63 @@ import config
 
 decim = 11  # do not touch this value unless you know what you are doing
 
+
 def run_ica(subject, tsss=config.mf_st_duration):
     print("Processing subject: %s" % subject)
 
     meg_subject_dir = op.join(config.meg_dir, subject)
 
-    epochs_fname = op.join(meg_subject_dir,
-                           config.base_epochs_fname.format(**locals()))
-    
-    epochs_for_ica_fname = op.splitext(epochs_fname)[0] + '_for_ica.fif'
+    raw_list = list()
+    events_list = list()
+    print("  Loading raw data")
 
-    epochs_for_ica = mne.read_epochs(epochs_for_ica_fname, preload=True)
-    
+    for run in config.runs:
+        run += '_filt_sss'
+        raw_fname = op.join(meg_subject_dir,
+                            config.base_raw_fname.format(**locals()))
+        eve_fname = op.splitext(raw_fname)[0] + '-eve.fif'
+
+        raw = mne.io.read_raw_fif(raw_fname, preload=True)
+        events = mne.read_events(eve_fname)
+        events_list.append(events)
+
+        raw.info['bads'] = config.bads[subject]
+        raw_list.append(raw)
+
+    raw, events = mne.concatenate_raws(raw_list, events_list=events_list)
+    raw.set_eeg_reference(projection=True)
+    del raw_list
+
+    # produce high-pass filtered version of the data for ICA
+    epochs_for_ica = mne.Epochs(raw.copy().filter(l_freq=1., h_freq=None),
+                                events, config.event_id, config.tmin,
+                                config.tmax, proj=True, baseline=config.baseline,
+                                preload=False, decim=config.decim,
+                                reject=config.reject)
+
     # run ICA on MEG and EEG
-    picks_meg = mne.pick_types(epochs_for_ica.info, meg=True, eeg=False,
+    picks_meg = mne.pick_types(raw.info, meg=True, eeg=False,
                                eog=False, stim=False, exclude='bads')
-    picks_eeg = mne.pick_types(epochs_for_ica.info, meg=False, eeg=True,
+    picks_eeg = mne.pick_types(raw.info, meg=False, eeg=True,
                                eog=False, stim=False, exclude='bads')
     all_picks = dict({'meg': picks_meg, 'eeg': picks_eeg})
 
     for ch_type in ['meg', 'eeg']:
         print('Running ICA for ' + ch_type)
-        
-        if ch_type == 'meg': 
-            
-            # XXX this does not work on epochs: 
+
+        if ch_type == 'meg':
+
+            # XXX this does not work on epochs:
             # meg_maxfilter_rank = epochs_for_ica.copy().pick_types(meg=True).estimate_rank()
-            n_components=0.999
-            
-        elif ch_type == 'eeg': 
-            
-            n_components=0.999
-        
+            n_components = 0.999
+
+        elif ch_type == 'eeg':
+
+            n_components = 0.999
+
         ica = ICA(method='fastica', random_state=config.random_state,
-              n_components=n_components)
-        
+                  n_components=n_components)
+
         picks = all_picks[ch_type]
 
         ica.fit(epochs_for_ica, picks=picks, decim=decim)

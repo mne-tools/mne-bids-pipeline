ea62bb961070a0b590cc232e116e8aaede7531a1
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2019-07-29 14:36:23 +0200

    individual dataset dirs and limited data to download

--- tests/download_test_data.py
+++ tests/download_test_data.py
@@ -6,26 +6,38 @@ import datalad.api as dl
 
 
 def _provide_testing_data():
-    """Return URLs to testing data."""
-    urls = [
-        'https://github.com/OpenNeuroDatasets/ds001810',
-        'https://github.com/OpenNeuroDatasets/ds001971',
-    ]
-    return urls
+    """Return dict of dataset, and the corresponding URLs."""
+    urls_dict = {
+        'ds001810': 'https://github.com/OpenNeuroDatasets/ds001810',
+        'ds001971': 'https://github.com/OpenNeuroDatasets/ds001971',
+    }
+    return urls_dict
 
 
-# Download the teting data
+def _provide_get_dict():
+    """Return dict of dataset, and which data to get from it."""
+    get_dict = {
+        'ds001810': 'sub-01/ses-anodalpre',
+        'ds001971': 'sub-001'
+    }
+    return get_dict
+
+
+# Download the testing data
 if __name__ == '__main__':
     # Save everything in ~/data
     data_dir = op.join(op.expanduser('~'), 'data')
     if not op.exists(data_dir):
         os.makedirs(data_dir)
 
-    urls = _provide_testing_data
+    urls_dict = _provide_testing_data()
+    get_dict = _provide_get_dict()
 
-    for url in urls:
+    for dsname, url in urls_dict.items():
+        dspath = op.join(data_dir, dsname)
+
         # install the dataset
-        dataset = dl.install(path=url, source=data_dir)
+        dataset = dl.install(path=dspath, source=url)
 
         # get the first subject
-        dataset.get('sub-01')
+        dataset.get(get_dict[dsname])

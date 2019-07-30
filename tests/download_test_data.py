467bb541c990a430f79a8140186e4ff6015e309d
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2019-07-30 16:31:36 +0200

    complete path

--- tests/download_test_data.py
+++ tests/download_test_data.py
@@ -21,7 +21,7 @@ def _provide_get_dict():
         'ds000117': ['sub-01/ses-meg/meg/sub-01_ses-meg_task-facerecognition_coordsystem.json',  # noqa: E501
                      'sub-01/ses-meg/meg/sub-01_ses-meg_task-facerecognition_run-01_events.tsv',  # noqa: E501
                      'sub-01/ses-meg/meg/sub-01_ses-meg_task-facerecognition_run-01_meg.fif',  # noqa: E501
-                     'sub-01_ses-meg_headshape.pos'
+                     'sub-01/ses-meg/meg/sub-01_ses-meg_headshape.pos'
                      ],
         'ds001810': ['sub-01/ses-anodalpre'],
         'ds001971': ['sub-001'],

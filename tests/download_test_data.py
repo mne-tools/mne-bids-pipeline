4605fa729d4811f41bd29f26768dfbbfaa01391f
Mainak Jas <mainakjas@gmail.com>
2019-08-03 10:44:22 -0400

    FIX: datalad does not work in parallel for new repo

--- tests/download_test_data.py
+++ tests/download_test_data.py
@@ -64,7 +64,9 @@ if __name__ == '__main__':
         print('datalad installing "{}"'.format(dsname))
         dataset = dl.install(path=dspath, source=url)
 
+        # XXX: git-annex bug: https://github.com/datalad/datalad/issues/3583
+        n_jobs = 1
         # get the first subject
         for to_get in get_dict[dsname]:
             print('datalad get data "{}" for "{}"'.format(to_get, dsname))
-            dataset.get(to_get)
+            dataset.get(to_get, jobs=n_jobs)

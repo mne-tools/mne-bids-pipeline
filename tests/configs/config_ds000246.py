df1c1fdcae6caca4726d245e3367749f3fc67e20
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2019-08-14 12:15:42 +0200

    add correct event_ids and descriptions to config file

--- tests/configs/config_ds000246.py
+++ tests/configs/config_ds000246.py
@@ -2,3 +2,5 @@ study_name = 'ds000246'
 runs = ['01']
 subjects_list = ['0001']
 reject = {'mag': 4e-12}
+conditions = ['standard', 'deviant', 'button']
+decoding_conditions = [('standard', 'deviant')]

0f3f9f10df6107fa1e58e8fd925e22442e5f123d
Richard Höchenberger <richard.hoechenberger@gmail.com>
2020-04-19 19:59:10 +0200

    Remove unused config file

--- tests/configs/config_ds000248_autobads.py
+++
@@ -1,9 +0,0 @@
-study_name = 'ds000248'
-subjects_list = ['01']
-conditions = ['Left']
-
-mf_ctc_fname = None
-mf_cal_fname = None
-find_flat_channels_meg = True
-find_noisy_channels_meg = True
-use_maxwell_filter = False

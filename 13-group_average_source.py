a60860f64dd99a8f0b8a3ef7f9b5ae6b55f5a5b3
Richard Höchenberger <richard.hoechenberger@gmail.com>
2020-06-05 15:20:40 +0200

    [MRG] BF: Don't alter MNE configuration (#142)
    * BF: Call set_config only once
    
    When processing multiple participants in parallel, one could encounter a
    race condition if multiple processes were trying to write to the MNE
    configuration file simultaneously. We now write the required setting
    only once, at the beginning of the script, before forking any
    sub-processes.
    
    * Remove mne config setting
    
    In fact, I believe we don't need this at all, since we're passing the
    subjects_dir to all functions that require it anyway.

--- 13-group_average_source.py
+++ 13-group_average_source.py
@@ -36,9 +36,6 @@ def morph_stc(subject, session=None):
                                        space=config.space
                                        )
 
-    mne.utils.set_config('SUBJECTS_DIR', config.get_fs_subjects_dir())
-    mne.datasets.fetch_fsaverage(subjects_dir=config.get_fs_subjects_dir())
-
     morphed_stcs = []
     for condition in config.conditions:
         method = config.inverse_method
@@ -71,6 +68,8 @@ def main():
     """Run grp ave."""
     msg = 'Running Step 13: Grand-average source estimates'
     logger.info(gen_log_message(step=13, message=msg))
+
+    mne.datasets.fetch_fsaverage(subjects_dir=config.get_fs_subjects_dir())
 
     parallel, run_func, _ = parallel_func(morph_stc, n_jobs=config.N_JOBS)
     all_morphed_stcs = parallel(run_func(subject, session)

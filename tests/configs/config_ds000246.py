943756b1929744d384d31e5a066a0f3436b99ccd
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2020-07-08 12:16:16 +0200

    better ds000246 (#151)
    * better ds000246
    
    * less memory

--- tests/configs/config_ds000246.py
+++ tests/configs/config_ds000246.py
@@ -1,10 +1,12 @@
 study_name = 'ds000246'
 runs = ['01']
+l_freq = .3
+h_freq = 100.
+decim = 4
 subjects_list = ['0001']
 ch_types = ['meg']
-reject = {'mag': 4e-12}
+reject = dict(mag=4e-12, eog=250e-6)
 conditions = ['standard', 'deviant', 'button']
 contrasts = [('deviant', 'standard')]
 decode = True
-crop = (100, 200)
 daysback = -365 * 110

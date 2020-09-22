22af4a20dcef8901bbbb7504829de0017deb422e
Richard Höchenberger <richard.hoechenberger@gmail.com>
2020-09-22 22:03:47 +0200

    Use MEG

--- tests/configs/config_ds000248_ica.py
+++ tests/configs/config_ds000248_ica.py
@@ -2,13 +2,14 @@
 MNE "sample" dataset: here we only process EEG data and check ICA.
 """
 study_name = 'MNE "sample" dataset'
-ch_types = ['eeg']
+ch_types = ['meg']
 data_type = 'meg'
 
 subjects_list = ['01']
 task = 'audiovisual'
 run = '01'
 l_freq = 0.3
+h_freq = 40.0
 
 conditions = ['Auditory/Left',
               'Auditory/Right',
@@ -18,10 +19,14 @@ conditions = ['Auditory/Left',
 tmin = -0.3
 tmax = 0.7
 baseline = (None, 0)
+reject = dict(mag=3000e-15,
+              grad=3000e-13)
+
 
 use_ssp = False
 use_ica = True
-ica_n_components = 0.99
-ica_l_freq = 1.
+ica_n_components = 0.80
+ica_l_freq = 1.0
+ica_max_iterations = 500
 
 interactive = False

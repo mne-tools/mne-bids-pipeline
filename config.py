acf917aabf01385f9403428ed49051a4d3de1d8a
Richard Höchenberger <richard.hoechenberger@gmail.com>
2020-12-18 17:24:34 +0100

    Remove loose, depth, smooth params; re-arrange docs

--- config.py
+++ config.py
@@ -744,27 +744,26 @@ mindist: float = 5
 Exclude points closer than this distance (mm) to the bounding surface.
 """
 
-loose: Union[float, Literal['auto']] = 0.2
-# ``loose`` : float in [0, 1] | 'auto'
-"""
-Value that weights the source variances of the dipole components
-that are parallel (tangential) to the cortical surface. If loose
-is 0 then the solution is computed with fixed orientation,
-and fixed must be True or "auto".
-If loose is 1, it corresponds to free orientations.
-The default value ('auto') is set to 0.2 for surface-oriented source
-space and set to 1.0 for volumetric, discrete, or mixed source spaces,
-unless ``fixed is True`` in which case the value 0. is used.
-"""
+# loose: Union[float, Literal['auto']] = 0.2
+# # ``loose`` : float in [0, 1] | 'auto'
+# """
+# Value that weights the source variances of the dipole components
+# that are parallel (tangential) to the cortical surface.If ``0``, then the
+# inverse solution is computed with **fixed orientation.**
+# If ``1``, it corresponds to **free orientation.**
+# The default value, ``'auto'``, is set to ``0.2`` for surface-oriented source
+# spaces, and to ``1.0`` for volumetric, discrete, or mixed source spaces,
+# unless ``fixed is True`` in which case the value 0. is used.
+# """
 
-depth: Optional[Union[float, dict]] = 0.8
-"""
-If float (default 0.8), it acts as the depth weighting exponent (``exp``)
-to use (must be between 0 and 1). None is equivalent to 0, meaning no
-depth weighting is performed. Can also be a `dict` containing additional
-keyword arguments to pass to :func:`mne.forward.compute_depth_prior`
-(see docstring for details and defaults).
-"""
+# depth: Optional[Union[float, dict]] = 0.8
+# """
+# If float (default 0.8), it acts as the depth weighting exponent (``exp``)
+# to use (must be between 0 and 1). None is equivalent to 0, meaning no
+# depth weighting is performed. Can also be a `dict` containing additional
+# keyword arguments to pass to :func:`mne.forward.compute_depth_prior`
+# (see docstring for details and defaults).
+# """
 
 inverse_method: Literal['MNE', 'dSPM', 'sLORETA', 'eLORETA'] = 'dSPM'
 """
@@ -807,13 +806,6 @@ covariance can ONLY be estimated from the pre-stimulus period.
     ```python
     noise_cov = 'emptyroom'
     ```
-"""
-
-smooth: Optional[int] = 10
-"""
-Number of iterations for the smoothing of the surface data.
-If None, smooth is automatically defined to fill the surface
-with non-zero values. The default is spacing=None.
 """
 
 ###############################################################################
---
+++ docs/source/settings/source/bem.md
@@ -0,0 +1,2 @@
+::: config.bem_creation_method
+::: config.recreate_bem

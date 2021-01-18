adc4b67e95e266b43e6cbe40f0b12ec21874847e
Richard Höchenberger <richard.hoechenberger@gmail.com>
2021-01-18 19:05:21 +0100

    Style

--- scripts/report/01-make_reports.py
+++ scripts/report/01-make_reports.py
@@ -466,13 +466,13 @@ def run_report_average(session):
             section='events'
         )
         css = ('.event-counts {\n'
-            '  display: -webkit-box;\n'
-            '  display: -ms-flexbox;\n'
-            '  display: -webkit-flex;\n'
-            '  display: flex;\n'
-            '  justify-content: center;\n'
-            '  text-align: center;\n'
-            '}')
+               '  display: -webkit-box;\n'
+               '  display: -ms-flexbox;\n'
+               '  display: -webkit-flex;\n'
+               '  display: flex;\n'
+               '  justify-content: center;\n'
+               '  text-align: center;\n'
+               '}')
         rep.add_custom_css(css)
 
     method = config.inverse_method

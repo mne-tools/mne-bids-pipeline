93d1ce364f768e3bfd16096f79c6603e2bd0a0fa
Richard Höchenberger <richard.hoechenberger@gmail.com>
2021-02-17 09:56:38 +0100

    Remove anonymization option
    Closes #159, #160

--- tests/configs/config_ds000246.py
+++ tests/configs/config_ds000246.py
@@ -15,5 +15,4 @@ reject = dict(mag=4e-12, eog=250e-6)
 conditions = ['standard', 'deviant', 'button']
 contrasts = [('deviant', 'standard')]
 decode = True
-daysback = -365 * 110
 on_error = 'debug'

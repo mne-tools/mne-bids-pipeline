a9d06680143af5d6e74657892a866ae778afe045
crsegerie <charbel-raphael.segerie@hotmail.fr>
2021-06-07 11:42:51 +0200

    typo

--- config.py
+++ config.py
@@ -91,7 +91,7 @@ The runs to process.
 
 exclude_runs: Optional[Dict[str, List[str]]] = None
 """
-Specify runs to exclude from analysis, for each participant invidiually.
+Specify runs to exclude from analysis, for each participant individually.
 
 ???+ example "Example"
     ```python

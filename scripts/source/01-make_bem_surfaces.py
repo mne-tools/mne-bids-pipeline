c1e9a6591d4c4a637e7bc2b5932c09340ed73605
Richard Höchenberger <richard.hoechenberger@gmail.com>
2021-06-11 11:27:06 +0200

    (Re)create high-res scalp surface
    Adds new config option `recreate_scalp_surface`
    
    I got error messages for some datasets when generating the report
    (coregistration figures) where it would complain about not being able to
    find the surfaces. This problem is gone with this PR.

--- scripts/source/01-make_bem_surfaces.py
+++ scripts/source/01-make_bem_surfaces.py
@@ -3,8 +3,11 @@
 Create BEM surfaces
 ===================
 
-Extract the BEM surfaces using the watershed algorithm. This is required to
-produce the forward solution in the next step.
+Generate the BEM surfaces from a T1 or FLASH MRI scan. This is required to
+produce the conductivity model and forward solution in the next step.
+
+This script will also create a high-resolution surface of the scalp, which can
+be used for visualization of the sensor alignment (coregistration).
 """
 
 import logging
@@ -44,10 +47,10 @@ def make_bem(subject):
         msg = 'Found existing BEM surfaces. '
         if config.recreate_bem:
             msg += 'Overwriting as requested in configuration.'
-            logger.info(gen_log_message(step=10, message=msg))
+            logger.info(gen_log_message(step=10, message=msg, subject=subject))
         else:
             msg = 'Skipping surface extraction as requested in configuration.'
-            logger.info(gen_log_message(step=10, message=msg))
+            logger.info(gen_log_message(step=10, message=msg, subject=subject))
             return
 
     if mri_images == 'FLASH':
@@ -58,7 +61,7 @@ def make_bem(subject):
                'watershed algorithm')
         bem_func = mne.bem.make_watershed_bem
 
-    logger.info(gen_log_message(step=10, message=msg))
+    logger.info(gen_log_message(step=10, message=msg, subject=subject))
     bem_func(subject=fs_subject,
              subjects_dir=fs_subjects_dir,
              copy=True,
@@ -66,9 +69,41 @@ def make_bem(subject):
              show=show)
 
 
+@failsafe_run(on_error=on_error)
+def make_scalp_surface(subject):
+    fs_subject = config.get_fs_subject(subject)
+    fs_subjects_dir = config.get_fs_subjects_dir()
+    bem_dir = Path(fs_subjects_dir) / fs_subject / 'bem'
+
+    generate_surface = config.recreate_scalp_surface
+
+    # Even if the user didn't ask for re-creation of the surface, we check if
+    # the required file exist; if it doesn't, we create it
+    surface_fname = bem_dir / f'sub-{subject}-head-dense.fif'
+    if not generate_surface and not surface_fname.exists():
+        generate_surface = True
+
+    if not generate_surface:
+        # Seems everything is in place, so we can safely skip surface creation
+        msg = 'Not generating high-resolution scalp surface.'
+        logger.info(gen_log_message(step=10, message=msg, subject=subject))
+        return
+
+    msg = 'Generating high-resolution scalp surface for coregistration.'
+    logger.info(gen_log_message(step=10, message=msg, subject=subject))
+
+    mne.bem.make_scalp_surfaces(
+        subject=fs_subject,
+        subjects_dir=fs_subjects_dir,
+        no_decimate=True,
+        force=True,
+        overwrite=True
+    )
+
+
 def main():
     """Run BEM surface extraction."""
-    msg = 'Running Step 10: Create BEM surfaces'
+    msg = 'Running Step 10: Create BEM & high-resolution scalp surface'
     logger.info(gen_log_message(step=10, message=msg))
 
     if not config.run_source_estimation:
@@ -79,7 +114,11 @@ def main():
     parallel, run_func, _ = parallel_func(make_bem, n_jobs=config.N_JOBS)
     parallel(run_func(subject) for subject in config.get_subjects())
 
-    msg = 'Completed Step 10: Create BEM surfaces'
+    parallel, run_func, _ = parallel_func(make_scalp_surface,
+                                          n_jobs=config.N_JOBS)
+    parallel(run_func(subject) for subject in config.get_subjects())
+
+    msg = 'Completed Step 10: Create BEM & high-resolution scalp surface'
     logger.info(gen_log_message(step=10, message=msg))
 
 
--- tests/configs/config_ds000248.py
+++ tests/configs/config_ds000248.py
@@ -24,6 +24,7 @@ noise_cov = 'emptyroom'
 
 bem_mri_images = 'FLASH'
 recreate_bem = True
+recreate_scalp_surface = True
 
 def mri_t1_path_generator(bids_path):
     # don't really do any modifications – just for testing!

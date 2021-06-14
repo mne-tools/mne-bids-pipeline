e32bbded8a69fd3a9600c8720ce0e414f2420fa5
Richard Höchenberger <richard.hoechenberger@gmail.com>
2021-06-14 20:36:02 +0200

    Add source group average

--- scripts/source/05-group_average.py
+++ scripts/source/05-group_average.py
@@ -10,41 +10,42 @@ import itertools
 import logging
 
 import mne
+from mne.utils._bunch import BunchConst
 from mne.parallel import parallel_func
 
 from mne_bids import BIDSPath
 
 import config
-from config import gen_log_message, on_error, failsafe_run
+from config import (gen_log_message, on_error, failsafe_run, get_fs_subject,
+                    sanitize_cond_name)
 
 logger = logging.getLogger('mne-bids-pipeline')
 
 
-def morph_stc(subject, session=None):
+def morph_stc(cfg, subject, session=None):
     bids_path = BIDSPath(subject=subject,
                          session=session,
-                         task=config.get_task(),
-                         acquisition=config.acq,
+                         task=cfg.task,
+                         acquisition=cfg.acq,
                          run=None,
-                         recording=config.rec,
-                         space=config.space,
-                         datatype=config.get_datatype(),
-                         root=config.get_deriv_root(),
+                         recording=cfg.rec,
+                         space=cfg.space,
+                         datatype=cfg.datatype,
+                         root=cfg.deriv_root,
                          check=False)
 
-    fs_subject = config.get_fs_subject(subject)
-    fs_subjects_dir = config.get_fs_subjects_dir()
+    fs_subject = get_fs_subject(subject)
 
     morphed_stcs = []
 
-    if isinstance(config.conditions, dict):
-        conditions = list(config.conditions.keys())
+    if isinstance(cfg.conditions, dict):
+        conditions = list(cfg.conditions.keys())
     else:
-        conditions = config.conditions
+        conditions = cfg.conditions
 
     for condition in conditions:
-        method = config.inverse_method
-        cond_str = config.sanitize_cond_name(condition)
+        method = cfg.inverse_method
+        cond_str = sanitize_cond_name(condition)
         inverse_str = method
         hemi_str = 'hemi'  # MNE will auto-append '-lh' and '-rh'.
         morph_str = 'morph2fsaverage'
@@ -67,57 +68,78 @@ def morph_stc(subject, session=None):
     return morphed_stcs
 
 
+def get_config(subject, session):
+    cfg = BunchConst(
+        task=config.get_task(),
+        datatype=config.get_datatype(),
+        acq=config.acq,
+        rec=config.rec,
+        space=config.space,
+        conditions=config.conditions,
+        inverse_method=config.inverse_method,
+        subjects=config.get_subjects(),
+        sessions=config.get_sessions(),
+        fs_subjects_dir=config.get_fs_subjects_dir(),
+        deriv_root=config.get_deriv_root(),
+        N_JOBS=config.N_JOBS,
+        run_source_estimation=config.run_source_estimation
+    )
+    return cfg
+
+
 @failsafe_run(on_error=on_error)
 def main():
     """Run group average in source space"""
     msg = 'Running Step 13: Grand-average source estimates'
     logger.info(gen_log_message(step=13, message=msg))
 
-    if not config.run_source_estimation:
+    cfg = get_config(None, None)
+
+    if not cfg.run_source_estimation:
         msg = '    … skipping: run_source_estimation is set to False.'
         logger.info(gen_log_message(step=13, message=msg))
         return
 
-    mne.datasets.fetch_fsaverage(subjects_dir=config.get_fs_subjects_dir())
+    mne.datasets.fetch_fsaverage(subjects_dir=cfg.fs_subjects_dir)
 
-    parallel, run_func, _ = parallel_func(morph_stc, n_jobs=config.N_JOBS)
+    parallel, run_func, _ = parallel_func(morph_stc, n_jobs=cfg.N_JOBS)
     all_morphed_stcs = parallel(run_func(subject, session)
                                 for subject, session in
-                                itertools.product(config.get_subjects(),
-                                                  config.get_sessions()))
+                                itertools.product(cfg.subjects,
+                                                  cfg.sessions))
     all_morphed_stcs = [morphed_stcs for morphed_stcs, subject in
-                        zip(all_morphed_stcs, config.get_subjects())]
+                        zip(all_morphed_stcs, cfg.subjects)]
     mean_morphed_stcs = map(sum, zip(*all_morphed_stcs))
 
     subject = 'average'
     # XXX to fix
-    if config.get_sessions():
-        session = config.get_sessions()[0]
+    if cfg.sessions:
+        session = cfg.sessions[0]
     else:
         session = None
 
     bids_path = BIDSPath(subject=subject,
                          session=session,
-                         task=config.get_task(),
-                         acquisition=config.acq,
+                         task=cfg.task,
+                         acquisition=cfg.acq,
                          run=None,
-                         processing=config.proc,
-                         recording=config.rec,
-                         space=config.space,
-                         datatype=config.get_datatype(),
-                         root=config.get_deriv_root(),
+                         processing=cfg.proc,
+                         recording=cfg.rec,
+                         space=cfg.space,
+                         datatype=cfg.datatype,
+                         root=cfg.deriv_root,
                          check=False)
 
-    if isinstance(config.conditions, dict):
-        conditions = list(config.conditions.keys())
+    if isinstance(cfg.conditions, dict):
+        conditions = list(cfg.conditions.keys())
     else:
-        conditions = config.conditions
+        conditions = cfg.conditions
 
     for condition, this_stc in zip(conditions, mean_morphed_stcs):
         this_stc /= len(all_morphed_stcs)
 
-        method = config.inverse_method
-        cond_str = config.sanitize_cond_name(condition)
+        method = cfg.inverse_method
+        cond_str = sanitize_cond_name(condition)
         inverse_str = method
         hemi_str = 'hemi'  # MNE will auto-append '-lh' and '-rh'.
         morph_str = 'morph2fsaverage'

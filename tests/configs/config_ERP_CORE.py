mne-tools/mne-study-template

Add autoreject (global) support

tests/configs/config_ERP_CORE.py
@@ -50,7 +50,7 @@ h_freq = None
 
 decode = True
 
-ica_reject = dict(eeg=350e-6, eog=500e-6)
+ica_reject = 'auto'
 reject = dict(eeg=150e-6)
 
 spatial_filter = 'ica'

mne-tools/mne-bids-pipeline

tests/configs/config_ERP_CORE.py
@@ -55,7 +55,7 @@ min_break_duration = 10
 t_break_annot_start_after_previous_event = 3.0
 t_break_annot_stop_before_next_event = 1.5
 
-ica_reject = dict(eeg=350e-6, eog=500e-6)
+ica_reject = 'autoreject_global'
 reject = dict(eeg=150e-6)
 
 spatial_filter = 'ica'
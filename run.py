783c7d2e948ed5c74c9802435360eb075dcf7f2e
Richard Höchenberger <richard.hoechenberger@gmail.com>
2021-07-16 10:34:07 +0200

    Fix incorrect step display in log output
    The issue we're seeing (processing steps are not displayed correctly / are
    outdated in the log) is due to the fact that we're using an environment
    variable, runpy, parallelization, combined with Python's caching of
    imports.
    
    Afer fiddling with this for quite a while and reading LOTS of Stack Overflow
    posts, I conclude that we cannot cleanly achieve our goal by setting an
    environment variable in run.py. We are not seeing similar issues with all
    the other variables we're using because they don't change over the course
    of the pipeline run. However, the MNE_BIDS_STUDY_SCRIPT_PATH is
    supposed to be updated with each new script that we call. This is why the
    issue surfaces there.
    
    My (interim?) solution is to write the path of the currently-executed script
    to disk, read it back from there in config.py, and set the environment
    variable accordingly – in config.py, too, and not in run.py as we currently
    do on `main`.
    
    This comes with the limitation that we can only have one pipeline instance
    run at a time, but I guess this is okay for now. There are certainly ways to
    resolve this and support multiple pipelines running in parallel, but I
    think we need to discuss this.
    
    WDYT?

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ import os
 import sys
 import runpy
 import pathlib
+import json
 import logging
 from typing import Union, Optional, Tuple
 if sys.version_info >= (3, 8):
@@ -13,6 +14,8 @@ else:
 
 import fire
 import coloredlogs
+import mne
+
 
 logger = logging.getLogger(__name__)
 
@@ -31,6 +34,9 @@ coloredlogs.install(
 
 PathLike = Union[str, pathlib.Path]
 
+MNE_CONFIG_DIR = pathlib.Path(mne.get_config_path()).parent
+MNE_CONFIG_DIR.mkdir(exist_ok=True)
+PIPELINE_SETTINGS_FILE = MNE_CONFIG_DIR / 'mne-bids-pipeline.json'
 
 INIT_SCRIPTS = ('00-init_derivatives_dir.py',)
 
@@ -99,7 +105,6 @@ def _run_script(script_path, config, root_dir, subject, session, task, run,
     # upon exit.
     env = os.environ
     env['MNE_BIDS_STUDY_CONFIG'] = str(pathlib.Path(config).expanduser())
-    env['MNE_BIDS_STUDY_SCRIPT_PATH'] = str(script_path)
 
     if root_dir:
         env['BIDS_ROOT'] = str(pathlib.Path(root_dir).expanduser())
@@ -234,11 +239,24 @@ def process(config: PathLike,
         "👋 Welcome aboard the MNE BIDS Pipeline!\n"
     )
     for script_path in script_paths:
+        with PIPELINE_SETTINGS_FILE.open('w', encoding='utf-8') as f:
+            settings = {
+                'current_script': str(script_path)
+            }
+            json.dump(obj=settings, fp=f)
+
         step_name = f'{script_path.parent.name}/{script_path.name}'
         logger.info(f'🚀 Now running script: {step_name} 👇')
         _run_script(script_path, config, root_dir, subject, session, task, run,
                     n_jobs)
         logger.info(f'🎉 Done running script: {step_name} 👏')
+
+    # Clear the settings
+    with PIPELINE_SETTINGS_FILE.open('w', encoding='utf-8') as f:
+        settings = {
+            'current_script': None
+        }
+        json.dump(obj=settings, fp=f)
 
 
 if __name__ == '__main__':

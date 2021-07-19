a9fcd54afd464c608d52bfdd6d9685b3992aba7b
Richard Höchenberger <richard.hoechenberger@gmail.com>
2021-07-19 18:46:54 +0200

    Add one line of output if we skip a step
    We now print "Skipping …" if a step is being skipped, so users can be
    certain there was no fluke.

--- scripts/preprocessing/04b-run_ssp.py
+++ scripts/preprocessing/04b-run_ssp.py
@@ -122,6 +122,8 @@ def get_config(
 def main():
     """Run SSP."""
     if not config.spatial_filter == 'ssp':
+        msg = 'Skipping …'
+        logger.info(**gen_log_kwargs(message=msg))
         return
 
     parallel, run_func, _ = parallel_func(run_ssp,

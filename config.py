1a28d1d6a80b2b38ef3183a11ed01225f4dd690f
crsegerie <charbel-raphael.segerie@hotmail.fr>
2021-07-23 15:45:55 +0200

    typo

--- config.py
+++ config.py
@@ -1234,8 +1234,8 @@ includes 10 subjects, the value of the 95th percentile of the t-distribution
 `t(10)` will be used for thresholding the clusters.
 
 Theoretically, you can tweak this value as you want to choose the size of
-the cluster, but for good science
-practices we advise to stick to our default value.
+the cluster, but for good science practices we advise to choose this value 
+preliminary to seing the results.
 """
 
 n_permutations: int = 10_000

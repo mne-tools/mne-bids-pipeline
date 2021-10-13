mne-tools/mne-bids-pipeline

commit 402bbd4bfac47f95faa87bda9e91dc71ea2f00fe

Only assemble epochs for the conditions of interest if specified (#449)
* Only assemble epochs for the conditions of interest if specified

* Pin setuptools to <58 to fix jsmin install

* Move setuptools up?

* Pin setuptools version in config.yml too

* Cleaner dep spec

* Add changelog entry [ci skip]

Co-authored-by: Richard Höchenberger <richard.hoechenberger@gmail.com>

scripts/preprocessing/03-make_epochs.py
@@ -88,6 +88,8 @@ def run_epochs(*, cfg, subject, session=None):
             event_repeated=cfg.event_repeated,
             decim=cfg.decim
         )
+        if config.conditions:
+            epochs = epochs[config.conditions]
         epochs_all_runs.append(epochs)
         del raw  # free memory
 

.circleci/config.yml
@@ -42,7 +42,7 @@ jobs:
       - run:
           name: Get Python running
           command: |
-            python -m pip install --user --upgrade --progress-bar off pip setuptools
+            python -m pip install --user --upgrade --progress-bar off pip "setuptools <58"
             python -m pip install --user --upgrade --progress-bar off -r tests/requirements.txt
       # - save_cache:
       #     key: pip-cache

commit 9153209577b83607746245cac935a9344b15d40f

Fix a bug introduced with #449 [circle full]

scripts/preprocessing/03-make_epochs.py
@@ -88,8 +88,13 @@ def run_epochs(*, cfg, subject, session=None):
             event_repeated=cfg.event_repeated,
             decim=cfg.decim
         )
-        if config.conditions:
-            epochs = epochs[config.conditions]
+
+        if isinstance(cfg.conditions, dict):
+            conditions = list(cfg.conditions.keys())
+        else:
+            conditions = cfg.conditions
+
+        epochs = epochs[conditions]
         epochs_all_runs.append(epochs)
         del raw  # free memory
 
@@ -133,6 +138,7 @@ def get_config(
         bids_root=config.get_bids_root(),
         deriv_root=config.get_deriv_root(),
         interactive=config.interactive,
+        conditions=config.conditions,
         epochs_tmin=config.epochs_tmin,
         epochs_tmax=config.epochs_tmax,
         epochs_metadata_tmin=config.epochs_metadata_tmin,
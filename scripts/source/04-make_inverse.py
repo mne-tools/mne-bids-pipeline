64d50d725d55434591a06eb2f2d331345f61a836
Apolline Mellot <apolline.mellot@inria.fr>
2021-10-19 09:47:41 +0200

    allow ad-hoc cov

--- scripts/source/04-make_inverse.py
+++ scripts/source/04-make_inverse.py
@@ -42,9 +42,12 @@ def run_inverse(*, cfg, subject, session=None):
     fname_cov = bids_path.copy().update(suffix='cov')
     fname_inv = bids_path.copy().update(suffix='inv')
 
-    cov = mne.read_cov(fname_cov)
-    forward = mne.read_forward_solution(fname_fwd)
     info = mne.io.read_info(fname_info)
+    if cfg.noise_cov == "ad-hoc":
+        cov = mne.make_ad_hoc_cov(info)
+    else:
+        cov = mne.read_cov(fname_cov)
+    forward = mne.read_forward_solution(fname_fwd)
     inverse_operator = make_inverse_operator(info, forward, cov, loose=0.2,
                                              depth=0.8, rank='info')
     write_inverse_operator(fname_inv, inverse_operator)
@@ -98,6 +101,7 @@ def get_config(
         space=config.space,
         source_info_path_update=config.source_info_path_update,
         inverse_targets=config.inverse_targets,
+        noise_cov=config.noise_cov,
         ch_types=config.ch_types,
         conditions=config.conditions,
         inverse_method=config.inverse_method,

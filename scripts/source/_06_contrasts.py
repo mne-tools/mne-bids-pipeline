ee9ab0484e36a94c3775f4641a54722815f4708a
Richard Höchenberger <richard.hoechenberger@gmail.com>
2021-12-01 10:55:52 +0100

    stash

--- scripts/source/_06_contrasts.py
+++ scripts/source/_06_contrasts.py
@@ -15,6 +15,7 @@ To then explore the results go to the source_analysis workspace
 which is in my local computer /home/charb/Desktop/parietal/other/source_analysis/source_visu.py
 """
 
+from pathlib import Path
 import itertools
 import logging
 from typing import Optional
@@ -35,9 +36,21 @@ from os.path import join
 logger = logging.getLogger('mne-bids-pipeline')
 
 # You have to  create the result folder beforehand
-res_path = "/storage/store2/data/time_in_wm_new/derivatives/source_contrast/res_3s_beta"
+freq_bands = {
+    'alpha': {
+        'range': (10, 15),
+        'outdir': Path('/storage/store2/derivatives/time_in_wm/source_contrast/res_3s_alpha')
+    },
+    'beta': {
+        'range': (15, 20),
+        'outdir': Path('/storage/store2/derivatives/time_in_wm/source_contrast/res_3s_beta')
+    }
 
-def fname(subject, session):
+}
+
+
+
+def fname(subject, session, res_path):
     """Get name of source file."""
     filename = f"brain_contrast_morphed_sub-{subject}-ses-{session}.stc"
     return join(res_path, filename)
@@ -45,14 +58,19 @@ def fname(subject, session):
 
 def plot_source(stc, filename):
     """Plot and save the source estimate."""
+    print('plotting source estimate :-)')
+    # return None
     brain = stc.plot(
         subjects_dir=config.get_fs_subjects_dir(),
         hemi="split", size=(1600, 800))
-    brain.save_image(filename=filename, mode='rgb')
+    print('... done!')
+
+    # brain.save_image(filename=filename, mode='rgb')
 
 
-def one_subject(subject, session, cfg):
+def one_subject(subject, session, cfg, freq_band):
     """Compute the contrast and morph it to the fsavg."""
+    print(f'processing subject {subject}')
     bids_path = BIDSPath(
         subject=subject,
         session=session,
@@ -85,61 +103,79 @@ def one_subject(subject, session, cfg):
         check=False)
 
     epochs = read_epochs(fname_epoch)
-    print("epochs loaded")
-    epochs.decimate(5)
+    # epochs.decimate(5)
+    # l_freq, h_freq = 15, 20
+    # l_freq, h_freq = 10, 15
+    l_freq, h_freq = freq_band['range']
+    res_path = freq_band['outdir']
+    res_path.mkdir(exist_ok=True)
 
     stc_cond = []
-    for cond in config.contrasts[0]:  # type: ignore
+    for cond in config.contrasts[0]:  # FIXME iterate over ALL contrasts
         print(cond)
-        l_freq, h_freq = 15, 20
 
-        epochs_filter: BaseEpochs = epochs[cond]  # type: ignore
-        data_epochs = epochs_filter.copy().crop(tmin=0, tmax=3)
-        data_epochs.filter(l_freq, h_freq)
+        # import ipdb
+        # ipdb.set_trace()
+        epochs_filter = epochs[cond].crop(tmin=1, tmax=4)
+        epochs_filter.filter(l_freq, h_freq)
 
-        data_cov = mne.compute_covariance(data_epochs)
+        data_cov = mne.compute_covariance(epochs_filter)
 
         # Here the inverse uses the empty room recording.
         stc_data = apply_inverse_cov(
             data_cov, epochs.info, inverse_operator,
             nave=len(epochs), method='dSPM', verbose=False)
-        print("subject", subject, np.max(stc_data.data), np.min(stc_data.data))
-        stc_data.data = np.log(stc_data.data)
+        stc_data.data = np.log10(stc_data.data)
         stc_cond.append(stc_data)
-        filename = f"brain_{cond}-sub-{subject}-ses-{session}.png"
+        filename = f"brain_{cond.replace('/', '+')}-sub-{subject}-ses-{session}.png"
+
+        # import ipdb
+        # ipdb.set_trace()
+
         plot_source(stc_data, join(res_path, filename))
 
     # Taking the difference of the log
+    # stc_cond[1].data = 10**stc_cond[1].data
+    # stc_cond[0].data = 10**stc_cond[0].data
+
+    # stc_contrast = stc_cond[1] / stc_cond[0]
     stc_contrast = stc_cond[1] - stc_cond[0]
 
-    filename = f"brain_contrast_sub-{subject}-ses-{session}.png"
-    plot_source(stc_contrast, join(res_path, filename))
+    # import ipdb
+    # ipdb.set_trace()
+    print("subject", subject, np.max(stc_contrast.data), np.min(stc_contrast.data))
 
+    # filename = f"brain_contrast_sub-{subject}-ses-{session}.png"
+    # plot_source(stc_contrast, join(res_path, filename))
+    
     morph = mne.compute_source_morph(
         stc_contrast,
         subject_from=config.get_fs_subject(subject), subject_to='fsaverage',
-        subjects_dir=cfg.fs_subjects_dir)
+        subjects_dir=cfg.fs_subjects_dir
+    )
     stc_fsaverage: SourceEstimate = morph.apply(stc_contrast)  # type: ignore
 
-    filename = f"brain_contrast_morphed_sub-{subject}-ses-{session}.png"
-    plot_source(stc_fsaverage, join(res_path, filename))
+    # filename = f"brain_contrast_morphed_sub-{subject}-ses-{session}.png"
+    # plot_source(stc_fsaverage, join(res_path, filename))
 
-    stc_fsaverage.save(fname=fname(subject, session))
+    stc_fsaverage.save(fname=fname(subject, session, res_path=res_path))
 
     return stc_fsaverage
 
 
-def group_analysis(subjects, sessions, cfg):
+def group_analysis(subjects, sessions, cfg, freq_band):
     """Take the average of the source estimates."""
+    res_path = freq_band['outdir']
+
     tab_stc = [[None for ses in sessions] for sub in subjects]
     for sub, subject in enumerate(subjects):
         for ses, session in enumerate(sessions):
             tab_stc[sub][ses] = mne.read_source_estimate(
-                fname=fname(subject, session), subject=subject).data
+                fname=fname(subject, session, res_path=res_path), subject=subject).data
 
     from mne.source_estimate import SourceEstimate
     stc_avg: SourceEstimate = mne.read_source_estimate(
-        fname=fname(subjects[0], sessions[0]))
+        fname=fname(subjects[0], sessions[0], res_path=res_path))
 
     # Average subject ###################################################
     # Save mean subject
@@ -153,7 +189,7 @@ def group_analysis(subjects, sessions, cfg):
     subject = "fsaverage"
     stc_avg.subject = subject
     brain = stc_avg.plot(
-        subjects_dir="/storage/store2/data/time_in_wm_new/derivatives/freesurfer/subjects",
+        subjects_dir="/storage/store2/derivatives/time_in_wm/freesurfer/subjects",
         hemi="split", size=(1600, 800), backend="pyvistaqt",
         colormap="seismic",
         # No need to calibrate the colorbar here, you can just use the visualization script
@@ -197,8 +233,12 @@ def get_config(
 
 def main():
     """Source space contrast."""
+    import os
+    os.environ['OMP_NUM_THREADS'] = str(config.get_n_jobs())
+    os.environ['NUMEXPR_NUM_THREADS'] = str(config.get_n_jobs())
+    os.environ['MKL_NUM_THREADS'] = str(config.get_n_jobs())
+
     subjects = config.get_subjects()
-    print(subjects)
     sessions = config.get_sessions()
     cfg = get_config()
 
@@ -209,11 +249,18 @@ def main():
     parallel, run_func, _ = parallel_func(one_subject,
                                           n_jobs=config.get_n_jobs())
     parallel(
-        run_func(cfg=cfg, subject=subject, session=session)
-        for subject, session in
-        itertools.product(subjects, sessions)
+        run_func(cfg=cfg, subject=subject, session=session, freq_band=freq_band)
+        for subject, session, freq_band in
+        itertools.product(subjects, sessions, [freq_bands['alpha'], freq_bands['beta']])
     )
-    group_analysis(subjects, sessions, cfg)
+
+    parallel, run_func, _ = parallel_func(group_analysis,
+                                          n_jobs=config.get_n_jobs())
+    parallel(
+        run_func(subjects, sessions, cfg, freq_band)
+        for freq_band in [freq_bands['alpha'], freq_bands['beta']]
+        # for subjects, sessions, cfg in subjects, sessions, cfg)
+    )
 
 
 if __name__ == '__main__':

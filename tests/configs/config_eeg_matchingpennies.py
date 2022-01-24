75905111544921322ef139b9447686f866e34090
Richard Höchenberger <richard.hoechenberger@gmail.com>
2022-01-24 09:54:41 +0100

    Update config [circle eeg_matchingpennies]

--- tests/configs/config_eeg_matchingpennies.py
+++ tests/configs/config_eeg_matchingpennies.py
@@ -12,7 +12,9 @@ ch_types = ['eeg']
 interactive = False
 reject = {'eeg': 150e-6}
 conditions = ['raised-left', 'raised-right']
-contrasts = [('raised-left', 'raised-right')]
+contrasts = [
+    ('raised-left', 'raised-right')
+]
 decode = True
 
 interpolate_bads_grand_average = False

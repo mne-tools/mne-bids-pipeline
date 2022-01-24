45a8ea485f1d4a1735cb1173d732a39f24afba00
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2022-01-24 12:13:07 +0100

    [test] fix conditions matchingpennies (#509)
    * fix conditions mp [circle eeg_matchingpennies]
    
    * Update config [circle eeg_matchingpennies]
    
    * fix typo [skip ci]
    
    Co-authored-by: Richard Höchenberger <richard.hoechenberger@gmail.com>

--- tests/configs/config_eeg_matchingpennies.py
+++ tests/configs/config_eeg_matchingpennies.py
@@ -11,8 +11,10 @@ task = 'matchingpennies'
 ch_types = ['eeg']
 interactive = False
 reject = {'eeg': 150e-6}
-conditions = ['left', 'right']
-contrasts = [('right', 'left')]
+conditions = ['raised-left', 'raised-right']
+contrasts = [
+    ('raised-left', 'raised-right')
+]
 decode = True
 
 interpolate_bads_grand_average = False

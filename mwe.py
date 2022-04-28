1b1604d11b09840eb5bb874e342243fc3173c506
Richard Höchenberger <richard.hoechenberger@gmail.com>
2022-04-28 09:42:28 +0200

    Update MWE run script

--- mwe.py
+++ mwe.py
@@ -1,10 +1,10 @@
 from run import process
 
 process(
-    # config='~/Development/mne-bids-pipeline/tests/configs/config_ds000247.py',
+    # config='~/Development/m   ne-bids-pipeline/tests/configs/config_ds000247.py',
     # config='~/Development/mne-bids-pipeline/tests/configs/config_ds000248_no_mri.py',
     config='/storage/store2/data/time_in_wm/code/bids_pipeline_config.py',
-    subject='220',
+    # subject='220,197',
     steps='sensor/time_frequency_csp',
-    n_jobs='1'
+    n_jobs='30'
 )

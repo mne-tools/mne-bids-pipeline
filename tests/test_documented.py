fbcd082b5f0a247ae091b17a0fa3a77c1c1fc34a
Eric Larson <larson.eric.d@gmail.com>
2022-08-20 22:56:17 -0400

    FIX: Formatting

--- tests/test_documented.py
+++ tests/test_documented.py
@@ -9,33 +9,35 @@ root_path = Path(__file__).parent.parent
 def test_documented():
     """Test that all options are suitably documented."""
     # use ast to parse config.py for assignments
-    with open(root_path / 'config.py', 'r') as fid:
+    with open(root_path / "config.py", "r") as fid:
         contents = fid.read()
     contents = ast.parse(contents)
     in_config = [
-        item.target.id for item in contents.body
-        if isinstance(item, ast.AnnAssign)]
+        item.target.id for item in contents.body if isinstance(item, ast.AnnAssign)
+    ]
     assert len(set(in_config)) == len(in_config)
     in_config = set(in_config)
-    settings_path = root_path / 'docs' / 'source' / 'settings'
+    settings_path = root_path / "docs" / "source" / "settings"
     in_doc = set()
-    key = '::: config.'
-    allowed_duplicates = set([
-        'source_info_path_update',
-    ])
+    key = "::: config."
+    allowed_duplicates = set(
+        [
+            "source_info_path_update",
+        ]
+    )
     for dirpath, _, fnames in os.walk(settings_path):
         for fname in fnames:
-            if not fname.endswith('.md'):
+            if not fname.endswith(".md"):
                 continue
             # This is a .md file
-            with open(Path(dirpath) / fname, 'r') as fid:
+            with open(Path(dirpath) / fname, "r") as fid:
                 for line in fid:
                     if not line.startswith(key):
                         continue
                     # The line starts with our magic key
-                    val = line[len(key):].strip()
+                    val = line[len(key) :].strip()
                     if val not in allowed_duplicates:
-                        assert val not in in_doc, 'Duplicate documentation'
+                        assert val not in in_doc, "Duplicate documentation"
                     in_doc.add(val)
-    assert in_doc.difference(in_config) == set(), 'Extra values in doc'
-    assert in_config.difference(in_doc) == set(), 'Values missing from doc'
+    assert in_doc.difference(in_config) == set(), "Extra values in doc"
+    assert in_config.difference(in_doc) == set(), "Values missing from doc"

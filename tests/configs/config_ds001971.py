52fd60150b3f5bdeca1476ec32795b2be28a0848
Eric Larson <larson.eric.d@gmail.com>
2022-09-13 07:28:10 -0700

    FIX: Better desc

--- tests/configs/config_ds001971.py
+++ tests/configs/config_ds001971.py
@@ -1,13 +1,6 @@
 """Mobile brain body imaging (MoBI) gait adaptation experiment.
 
-Set the `MNE_BIDS_STUDY_CONFIG` environment variable to
-"config_ds001971" to overwrite `config.py` with the values specified
-below.
-
-Download ds001971 from OpenNeuro: https://github.com/OpenNeuroDatasets/ds001971
-
-export MNE_BIDS_STUDY_CONFIG=config_ds001971
-export BIDS_ROOT=~/mne_data/ds001971
+See ds001971 on OpenNeuro: https://github.com/OpenNeuroDatasets/ds001971
 """
 
 

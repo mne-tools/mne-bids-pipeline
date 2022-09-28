470f6698f8f5165faf43117e56a26afed552a78d
Eric Larson <larson.eric.d@gmail.com>
2022-09-28 17:32:30 -0400

    FIX: Name

--- tests/test_documented.py
+++ tests/test_documented.py
@@ -2,6 +2,7 @@
 import ast
 from pathlib import Path
 import os
+import re
 import sys
 import yaml
 
@@ -51,8 +52,6 @@ def test_options_documented():
     assert in_config.difference(in_doc) == set(), "Values missing from doc"
 
 
-
-
 def test_datasets_in_doc():
     """Test that all datasets in tests are in the doc."""
     # There are four things to keep in sync:
@@ -66,7 +65,8 @@ def test_datasets_in_doc():
 
     # 1. Read cache, test, etc. entries from CircleCI
     with open(root_path / '.circleci' / 'config.yml', 'r') as fid:
-        circle_yaml = yaml.safe_load(fid.read())
+        circle_yaml_src = fid.read()
+    circle_yaml = yaml.safe_load(circle_yaml_src)
     caches = [
         job[6:] for job in circle_yaml['jobs'] if job.startswith('cache_')
     ]
@@ -77,9 +77,55 @@ def test_datasets_in_doc():
     ]
     assert len(tests) == len(set(tests))
     tests = set(tests)
-    # TODO: Go through circle_yaml['workflows']['commit']['jobs'] and
-    # make sure everything is consitent there. It's going to be a bit of a
-    # pain but probably worth it to be safe...
+    # Rather than going circle_yaml['workflows']['commit']['jobs'] and
+    # make sure everything is consitent there (too much work), let's at least
+    # check that we get the correct number using `.count`.
+    counts = dict(ERP_CORE=7, ds000248=6)
+    counts_noartifact = dict(ds000248=3)  # 3 are actually tests, not for docs
+    for name in sorted(caches):
+        get = f'Get {name}'
+        n_found = circle_yaml_src.count(get)
+        assert n_found == 1, get
+        dl = f'download_test_data.py -d {name}'
+        n_found = circle_yaml_src.count(dl)
+        assert n_found == 1, dl
+        # jobs: save_cache:
+        sc = f'key: data-cache-{name}'
+        n_found = circle_yaml_src.count(sc)
+        assert n_found == 1, sc
+        # jobs: restore_cache:
+        rc = f'- data-cache-{name}-'
+        n_found = circle_yaml_src.count(rc)
+        count = counts.get(name, 1)
+        assert n_found == count, f'{rc} ({n_found} != {count})'
+        # jobs: save_cache: paths:
+        pth = f'- ~/mne_data/{name}'
+        n_found = circle_yaml_src.count(pth)
+        assert n_found == 1, pth
+        # jobs:
+        cj = f'  cache_{name}:'
+        n_found = circle_yaml_src.count(cj)
+        assert n_found == 1, cj
+        tj = f'  test_{name}'
+        n_found = circle_yaml_src.count(tj)
+        assert n_found == count, f'{tj} ({n_found} != {count})'
+        # jobs: test_*: steps: store_artifacts
+        sa = f'path: /home/circleci/reports/{name}'
+        n_found = circle_yaml_src.count(sa)
+        this_count = count - counts_noartifact.get(name, 0)
+        assert n_found == this_count, f'{sa} ({n_found} != {this_count})'
+        # jobs: test_*: steps: persist_to_workspace
+        pw = re.compile(f'- mne_data/derivatives/mne-bids-pipeline/{name}[^\\.]+\\*.html')  # noqa: E501
+        n_found = len(pw.findall(circle_yaml_src))
+        assert n_found == this_count, f'{pw} ({n_found} != {this_count})'
+        # jobs: test_*: steps: run test
+        cp = re.compile(f"""\
+            DS={name}.*
+            python tests/run_tests.py --download=0 \\${{DS}}.*
+            mkdir -p ~/reports/\\${{DS}}
+            cp -av ~/mne_data/derivatives/mne-bids-pipeline/\\${{DS}}/[^\\.]+.html""")  # noqa: E501
+        n_found = len(cp.findall(circle_yaml_src))
+        assert n_found == this_count, f'{cp} ({n_found} != {this_count})'
 
     # 3. Read examples from docs (being careful about tags we can't read)
     class SafeLoaderIgnoreUnknown(yaml.SafeLoader):

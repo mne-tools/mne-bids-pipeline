4252c042fc9f50076654fdafaa0e6a5151f6a26b
Eric Larson <larson.eric.d@gmail.com>
2022-10-14 20:36:22 -0400

    FIX: Annotate

--- mne_bids_pipeline/_io.py
+++ mne_bids_pipeline/_io.py
@@ -1,18 +1,25 @@
 """I/O helpers."""
 
 import json_tricks
+from types import SimpleNamespace
+from mne_bids import BIDSPath
+
+from ._typing import PathLike
 
 
-def _write_json(fname, data):
+def _write_json(fname: PathLike, data: dict) -> None:
     with open(fname, 'w') as f:
         json_tricks.dump(data, fp=f, allow_nan=True, sort_keys=False)
 
 
-def _read_json(fname):
+def _read_json(fname: PathLike) -> dict:
     with open(fname, 'r') as f:
         return json_tricks.load(f)
 
 
-def _empty_room_match_path(run_path, cfg):
+def _empty_room_match_path(
+    run_path: BIDSPath,
+    cfg: SimpleNamespace
+) -> BIDSPath:
     return run_path.copy().update(
         extension='.json', suffix='emptyroommatch', root=cfg.deriv_root)

93d2aedbc27fb27b638e85d0edf2a17601870503
Eric Larson <larson.eric.d@gmail.com>
2022-11-10 21:00:39 -0500

    FIX: Root

--- mne_bids_pipeline/tests/configs/config_ds000248_base.py
+++ mne_bids_pipeline/tests/configs/config_ds000248_base.py
@@ -5,7 +5,7 @@ import mne
 
 study_name = 'ds000248'
 bids_root = '~/mne_data/ds000248'
-deriv_root = '~/mne_data/derivatives/mne-bids-pipeline/ds000248'
+deriv_root = '~/mne_data/derivatives/mne-bids-pipeline/ds000248_base'
 subjects_dir = f'{bids_root}/derivatives/freesurfer/subjects'
 
 subjects = ['01']

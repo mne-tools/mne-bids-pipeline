5ebe9483f87d7bf00fd3f3c4afc4ccd36f3b08eb
Eric Larson <larson.eric.d@gmail.com>
2022-12-01 14:54:53 -0500

    DOC: Changes (#674)

--- .circleci/setup_bash.sh
+++ .circleci/setup_bash.sh
@@ -1,13 +1,12 @@
-#!/bin/bash -ef
+#!/bin/bash -e
+
+set -o pipefail
 
 # skip job if not in a [circle full] or on main
-export REGEXP="r'(?<=\[circle )(.*)(?=\])'"
 export COMMIT_MESSAGE=$(git log --format=oneline -n 1);
 COMMIT_MESSAGE_ESCAPED=${COMMIT_MESSAGE//\'/\\\'}  # escape '
 export COMMIT_MESSAGE_ESCAPED=${COMMIT_MESSAGE_ESCAPED//\"/\\\'}  # escape "
-export CIRCLE_REQUESTED_JOB=$(
-    python -c "import re; matches = re.findall(pattern=${REGEXP}, string='${COMMIT_MESSAGE_ESCAPED}'); match = '' if not matches else matches[0]; print(match)"
-);
+export CIRCLE_REQUESTED_JOB=$(echo $COMMIT_MESSAGE_ESCAPED | sed -nE 's/^.*\[circle ([^]]+)\].*$/\1/p')
 
 echo "CIRCLE_JOB=$CIRCLE_JOB"
 echo "COMMIT_MESSAGE=$COMMIT_MESSAGE"
@@ -51,6 +50,7 @@ if [[
     ]]; then
         echo "Skip detected, exiting job ${CIRCLE_JOB} for PR ${CIRCLE_PULL_REQUEST}."
         circleci-agent step halt
+        exit 0
 # Otherwise, run everything
 elif [[ -v CIRCLE_PULL_REQUEST ]]; then
     echo "Running job ${CIRCLE_JOB} for PR ${CIRCLE_PULL_REQUEST}"
@@ -68,7 +68,6 @@ echo "export DOWNLOAD_DATA=\"python -m mne_bids_pipeline._download\"" >> $BASH_E
 # Similar CircleCI setup to mne-python (Xvfb, venv, minimal commands, env vars)
 wget -q https://raw.githubusercontent.com/mne-tools/mne-python/main/tools/setup_xvfb.sh
 bash setup_xvfb.sh
-sudo apt update
 sudo apt install -qq tcsh git-annex-standalone python3.10-venv python3-venv libxft2
 python3.10 -m venv ~/python_env
 wget -q https://raw.githubusercontent.com/mne-tools/mne-python/main/tools/get_minimal_commands.sh

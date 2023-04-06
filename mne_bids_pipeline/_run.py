d148ada2625b2caf2488d8bbd00495e08d9d91f7
Eric Larson <larson.eric.d@gmail.com>
2023-04-06 07:56:24 -0600

    MAINT: Dont use deprecated book API (#736)

--- mne_bids_pipeline/_run.py
+++ mne_bids_pipeline/_run.py
@@ -13,9 +13,9 @@ import time
 from typing import Callable, Optional, Dict, List
 from types import SimpleNamespace
 
+from filelock import FileLock
 from joblib import Memory
 import json_tricks
-from openpyxl import load_workbook
 import pandas as pd
 from mne_bids import BIDSPath
 
@@ -301,26 +301,16 @@ def save_logs(*, config: SimpleNamespace, logs) -> None:  # TODO add type
 
     df = df[columns]
 
-    if fname.exists():
-        book = None
-        try:
-            book = load_workbook(fname)
-        except Exception:  # bad file
-            pass
-        else:
-            if sheet_name in book:
-                book.remove(book[sheet_name])
-        writer = pd.ExcelWriter(fname, engine="openpyxl")
-        if book is not None:
-            try:
-                writer.book = book
-            except Exception:
-                pass  # AttributeError: can't set attribute 'book' (?)
-    else:
-        writer = pd.ExcelWriter(fname, engine="openpyxl")
-
-    with writer:
-        df.to_excel(writer, sheet_name=sheet_name, index=False)
+    with FileLock(fname.with_suffix(fname.suffix + ".lock")):
+        append = fname.exists()
+        writer = pd.ExcelWriter(
+            fname,
+            engine="openpyxl",
+            mode="a" if append else "w",
+            if_sheet_exists="replace" if append else None,
+        )
+        with writer:
+            df.to_excel(writer, sheet_name=sheet_name, index=False)
 
 
 def _update_for_splits(

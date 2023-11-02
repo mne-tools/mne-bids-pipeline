66064577d40b82135846f60fa44b1144295da002
Richard Höchenberger <richard.hoechenberger@gmail.com>
2023-11-02 15:25:00 +0100

    Add changelog and improve docstrings

--- mne_bids_pipeline/_config.py
+++ mne_bids_pipeline/_config.py
@@ -1381,7 +1381,8 @@ will generate a dictionary with (hopefully!) optimal thresholds for each
 channel type.
 
 If `"autoreject_local"`, use "local" `autoreject` to detect (and potentially repair) bad
-channels in each epoch.
+channels in each epoch. Use [`autoreject_n_interpolate`][mne_bids_pipeline._config.autoreject_n_interpolate]
+to control how many sensors are allowed to be bad before an epoch gets dropped.
 
 ???+ example "Example"
     ```python

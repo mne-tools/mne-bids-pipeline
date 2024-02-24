7410306ea6e240ecde470235a02f3ebd2f3119a8
Eric Larson <larson.eric.d@gmail.com>
2024-02-23 20:07:42 -0500

    FIX: More

--- mne_bids_pipeline/_run.py
+++ mne_bids_pipeline/_run.py
@@ -379,15 +379,18 @@ def _prep_out_files(
     exec_params: SimpleNamespace,
     out_files: dict[str, BIDSPath],
     check_relative: Optional[pathlib.Path] = None,
+    bids_only: bool = True,
 ):
     if check_relative is None:
         check_relative = exec_params.deriv_root
     for key, fname in out_files.items():
         # Sanity check that we only ever write to the derivatives directory
-        assert isinstance(fname, BIDSPath), type(fname)  # only write BIDSPath
-        if fname.suffix not in ("raw", "epochs"):
-            # raw and epochs can split on write, and .save should check for us
-            assert fname.split is None, fname.split
+        if bids_only:
+            assert isinstance(fname, BIDSPath), (type(fname), fname)
+        if isinstance(fname, BIDSPath):
+            if fname.suffix not in ("raw", "epochs"):
+                # raw and epochs can split on write, and .save should check for us
+                assert fname.split is None, fname.split
         fname = pathlib.Path(fname)
         if not fname.is_relative_to(check_relative):
             raise RuntimeError(

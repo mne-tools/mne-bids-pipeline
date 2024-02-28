854e733e3c85f8725d0e068e4e55fa923206ae0e
Eric Larson <larson.eric.d@gmail.com>
2024-02-28 16:31:28 -0500

    FIX: Name

--- mne_bids_pipeline/steps/preprocessing/_09_ptp_reject.py
+++ mne_bids_pipeline/steps/preprocessing/_09_ptp_reject.py
@@ -248,7 +248,7 @@ def get_config(
         random_state=config.random_state,
         ch_types=config.ch_types,
         _epochs_split_size=config._epochs_split_size,
-        image_kwargs=config.report_add_epochs_image_kwargs,
+        report_add_epochs_image_kwargs=config.report_add_epochs_image_kwargs,
         **_bids_kwargs(config=config),
     )
     return cfg

2004908df69038969af11bc38c08e75236f70fb5
SophieHerbst <ksherbst@gmail.com>
2024-04-02 12:58:21 +0200

    fix dict

--- mne_bids_pipeline/steps/source/_04_make_forward.py
+++ mne_bids_pipeline/steps/source/_04_make_forward.py
@@ -107,9 +107,9 @@ def get_input_fnames_forward(*, cfg, subject, session):
     if cfg.source_info_path_update is None:
         if cfg.noise_cov == "rest" | cfg.noise_cov == "noise":
             source_info_path_update = dict(
-                'processing'='clean',
-                'suffix'='raw',
-                'task'=cfg.noise_cov
+                processing='clean',
+                suffix='raw',
+                task=cfg.noise_cov
                 )
         else:
             source_info_path_update = dict(suffix="ave")

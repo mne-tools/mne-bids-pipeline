447f0c0f4924bbc35e89f22c3c71ee212e4340db
Eric Larson <larson.eric.d@gmail.com>
2024-04-18 15:09:41 -0400

    FIX: main

--- mne_bids_pipeline/tests/test_documented.py
+++ mne_bids_pipeline/tests/test_documented.py
@@ -76,6 +76,9 @@ def test_config_options_used():
         config_names.add(key)
     for key in _EXECUTION_OPTIONS:
         config_names.remove(key)
+    # Used in main only
+    for key in ("spatial_filter_raw",):
+        config_names.remove(key)
     pcs = _ParseConfigSteps(force_empty=())
     missing_from_config = sorted(set(pcs.steps) - config_names)
     assert missing_from_config == [], f"Missing from config: {missing_from_config}"

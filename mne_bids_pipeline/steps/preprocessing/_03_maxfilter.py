d5b743b5d24e89571831cd49e640063b6a6ba910
Harrison Ritz <harrison.ritz@gmail.com>
2025-02-26 16:07:33 -0500

    allow for only mag channel types from `compute_rank`

--- mne_bids_pipeline/steps/preprocessing/_03_maxfilter.py
+++ mne_bids_pipeline/steps/preprocessing/_03_maxfilter.py
@@ -468,8 +468,9 @@ def run_maxwell_filter(
 
         bids_path_ref_sss = in_files.pop("raw_ref_run_sss")
         raw_exp = mne.io.read_raw_fif(bids_path_ref_sss)
-        rank_exp = mne.compute_rank(raw_exp, rank="info")["meg"]
-        rank_noise = mne.compute_rank(raw_sss, rank="info")["meg"]
+        type_sel = set(cfg.ch_types).intersection({'mag', 'meg'}).pop()
+        rank_exp = mne.compute_rank(raw_exp, rank="info")[type_sel]
+        rank_noise = mne.compute_rank(raw_sss, rank="info")[type_sel]
         del raw_exp
 
         if task == "rest":

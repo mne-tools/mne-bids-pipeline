e74dd3824950692eba1d04bcf527cf2d9d1e55e1
Eric Larson <larson.eric.d@gmail.com>
2025-06-08 17:36:55 -0400

    Update mne_bids_pipeline/tests/configs/config_ds000117.py

--- mne_bids_pipeline/tests/configs/config_ds000117.py
+++ mne_bids_pipeline/tests/configs/config_ds000117.py
@@ -20,7 +20,7 @@ process_empty_room = True
 mf_reference_run = "02"
 mf_cal_fname = bids_root + "/derivatives/meg_derivatives/sss_cal.dat"
 mf_ctc_fname = bids_root + "/derivatives/meg_derivatives/ct_sparse.fif"
-mf_int_order = 7
+mf_int_order = 9
 mf_ext_order = 4
 
 reject = {"grad": 4000e-13, "mag": 4e-12}

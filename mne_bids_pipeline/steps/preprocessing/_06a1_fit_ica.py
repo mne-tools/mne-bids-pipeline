c7d6f0729d3d4978944601c7d9e1edd42f5d2a55
jschepers <judith.schepers@vis.uni-stuttgart.de>
2026-02-17 13:35:29 +0100

    Revert changes to ica picks

--- mne_bids_pipeline/steps/preprocessing/_06a1_fit_ica.py
+++ mne_bids_pipeline/steps/preprocessing/_06a1_fit_ica.py
@@ -315,8 +315,7 @@ def run_ica(
         fit_params=fit_params,
         max_iter=cfg.ica_max_iterations,
     )
-    # TODO: Check whether `cfg.ch_types` is the best solution since it only includes eeg/meg channels not e.g. eog
-    ica.fit(epochs, picks=cfg.ch_types, decim=cfg.ica_decim)
+    ica.fit(epochs, decim=cfg.ica_decim)
     explained_var = (
         ica.pca_explained_variance_[: ica.n_components_].sum()
         / ica.pca_explained_variance_.sum()

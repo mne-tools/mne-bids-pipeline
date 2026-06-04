da0d57448673f69fd9f7e387d9d26cc13facfa52
Eric Larson <larson.eric.d@gmail.com>
2026-06-03 21:53:49 -0400

    FIX: Limit

--- .circleci/install_deps.sh
+++ .circleci/install_deps.sh
@@ -6,7 +6,7 @@ pip install --upgrade --progress-bar off -v \
     "autoreject @ https://api.github.com/repos/autoreject/autoreject/zipball/master" \
     "mne[hdf5] @ https://api.github.com/repos/mne-tools/mne-python/zipball/main" \
     "mne-bids[full] @ https://api.github.com/repos/mne-tools/mne-bids/zipball/main" \
-    "openneuro-py @ https://api.github.com/repos/openneuro-py/openneuro-py/zipball/main" \
+    "openneuro-py @ https://api.github.com/repos/larsoner/openneuro-py/zipball/max5" \
     numba \
     onnxruntime \
     PySide6 \

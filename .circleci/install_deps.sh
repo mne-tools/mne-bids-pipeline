d8f9709a9b9b186e6a50e7c062a0836cb14ddf97
Eric Larson <larson.eric.d@gmail.com>
2026-06-08 18:18:56 -0400

    Update install_deps.sh

--- .circleci/install_deps.sh
+++ .circleci/install_deps.sh
@@ -2,9 +2,8 @@
 set -exo pipefail
 
 pip install --upgrade --progress-bar off pip setuptools
-# TODO: Revert this optimizer here!
 pip install --upgrade --progress-bar off -v \
-    "autoreject @ https://api.github.com/repos/larsoner/autoreject/zipball/optimizer" \
+    "autoreject @ https://api.github.com/repos/autoreject/autoreject/zipball/master" \
     "mne[hdf5] @ https://api.github.com/repos/mne-tools/mne-python/zipball/main" \
     "mne-bids[full] @ https://api.github.com/repos/mne-tools/mne-bids/zipball/main" \
     "openneuro-py @ https://api.github.com/repos/larsoner/openneuro-py/zipball/max5" \

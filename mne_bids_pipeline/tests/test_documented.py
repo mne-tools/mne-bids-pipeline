705bd675d69e1590033902e536b9dbc24da1a363
nordme <nordme@uw.edu>
2026-09-04 16:38:32 -0700

    test documented fix

--- mne_bids_pipeline/tests/test_documented.py
+++ mne_bids_pipeline/tests/test_documented.py
@@ -274,7 +274,7 @@ def test_datasets_in_doc() -> None:
 
     # Some have been split into multiple test runs, so trim down to the same
     # set as caches
-    for key in ("ERP_CORE", "ds000248"):
+    for key in ("ERP_CORE", "ds000248", "ds003392"):
         tests = set(
             job if not job.startswith(key) else job[: len(key)] for job in tests
         )
